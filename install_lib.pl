03fd52645473a4061982b0767d3e55004d29401f
Dmitriy Kuminov <coding@dmik.org>
2017-03-31 19:58:27 +0000

    perl: Fix link->copy fallback in install_lib.pl.
    This is a regression which was fixed upstream in 2014 (but our perl is still not there).
    See https://rt.perl.org/Public/Bug/Display.html?id=123141.
    
    git-svn-id: http://svn.netlabs.org/repos/ports/perl/trunk@2167 8d6eb4b1-0faa-de11-b929-00e081727c95

--- install_lib.pl
+++ install_lib.pl
@@ -7,6 +7,7 @@ use strict;
 use vars qw($Is_VMS $Is_W32 $Is_OS2 $Is_Cygwin $Is_Darwin $Is_NetWare
 	    %opts $packlist);
 use subs qw(unlink link chmod);
+require File::Copy;
 
 use Config;
 BEGIN {
